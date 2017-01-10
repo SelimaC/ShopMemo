be7c0e5d5741131c3dd76566c4d80cbde3f43f44
SelimaCurci <seliii@hotmail.it>
2017-01-10 20:03:06 +0100

    immagini

--- app/src/main/java/com/example/selima/shopmemo/Home_Activity.java
+++ app/src/main/java/com/example/selima/shopmemo/Home_Activity.java
@@ -10,11 +10,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -128,6 +130,7 @@ public class Home_Activity extends AppCompatActivity implements View.OnClickList
 
             }
         });
+
 
         productList = ProductFactory.getInstance(this).getAllProducts();
         ((AllFragment)(adapter.getItem(0))).setList(productList);
