9f8c91b922633a32650ef5b32856b3526bcbf3a5
SelimaCurci <seliii@hotmail.it>
2017-01-14 09:40:36 +0100

    card per combo

---
+++ app/src/main/java/com/example/selima/shopmemo/SummaryCombo_Activity.java
@@ -0,0 +1,13 @@
+package com.example.selima.shopmemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryCombo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_combo_);
+    }
+}
