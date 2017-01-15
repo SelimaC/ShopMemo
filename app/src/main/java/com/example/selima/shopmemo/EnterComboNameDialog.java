a73fa266f1bda9c7f63bdd96d8a35f800f80e9af
f-fanni <fanni.filippo@gmail.com>
2017-01-15 16:33:58 +0100

    Forza l'inserimento del nome della combo in creazione

--- app/src/main/java/com/example/selima/shopmemo/EnterComboNameDialog.java
+++ app/src/main/java/com/example/selima/shopmemo/EnterComboNameDialog.java
@@ -8,13 +8,14 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
  * Created by filippo on 15/01/17.
  */
 public class EnterComboNameDialog extends DialogFragment {
-
+    View view;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -23,18 +24,22 @@ public class EnterComboNameDialog extends DialogFragment {
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        final View view = inflater.inflate(R.layout.enter_combo_name, null);
+        view = inflater.inflate(R.layout.enter_combo_name, null);
+        final View view = this.view;
         builder.setView(view)
         // Add action buttons
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                    @Override
                    public void onClick(DialogInterface dialog, int id) {
-                       Intent i = new Intent(getActivity().getApplicationContext(), NewCombo_Activity.class);
-                       EditText editText = (EditText) view.findViewById(R.id.nomeCombo);
+                       /*EditText editText = (EditText) view.findViewById(R.id.nomeCombo);
                        String nomeCombo = editText.getText().toString();
-                       i.putExtra("NOME", nomeCombo);
-
-                       startActivity(i);
+                       if (nomeCombo == null || nomeCombo.equals("")){
+                           editText.setError("Inserire il nome");
+                       }else {
+                           Intent i = new Intent(getActivity().getApplicationContext(), NewCombo_Activity.class);
+                           i.putExtra("NOME", nomeCombo);
+                           startActivity(i);
+                       }*/
                    }
                 })
                 .setNegativeButton("annulla", new DialogInterface.OnClickListener() {
@@ -43,5 +48,34 @@ public class EnterComboNameDialog extends DialogFragment {
                 })
                 .setTitle("Inserisci il nome");
         return builder.create();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();    //super.onStart() is where dialog.show() is actually called on the underlying dialog, so we have to do it after this point
+        final AlertDialog d = (AlertDialog)getDialog();
+        if(d != null)        {
+            Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Boolean wantToCloseDialog = false;
+                    //Do stuff, possibly set wantToCloseDialog to true then...
+                    EditText editText = (EditText) view.findViewById(R.id.nomeCombo);
+                    String nomeCombo = editText.getText().toString();
+                    if (nomeCombo == null || nomeCombo.equals("")){
+                        editText.setError("Inserire il nome");
+                    }else {
+                        wantToCloseDialog = true;
+                    }
+                    if(wantToCloseDialog){
+                        d.dismiss();
+                        Intent i = new Intent(getActivity().getApplicationContext(), NewCombo_Activity.class);
+                        i.putExtra("NOME", nomeCombo);
+                        startActivity(i);
+                    }
+                }
+            });
+        }
     }
 }
